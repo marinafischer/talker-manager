16335ee70267b2251bef3cfa4bb6ffe35a84f702
marinafischer <marinamirandafischer@gmail.com>
2022-04-25 15:21:56 -0300

    feat: req2

--- talkerRoute.js
+++ talkerRoute.js
@@ -3,6 +3,14 @@ const getTalker = require('./helpers/getTalker');
 
 const router = express.Router();
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await getTalker();
+  const findTalker = talker.find((t) => t.id === +id);
+  if (findTalker) return res.status(200).json(findTalker);
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
 router.get('/', async (req, res) => {
   const talker = await getTalker();
   res.status(200).json(talker);
